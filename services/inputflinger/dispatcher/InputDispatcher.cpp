f32b79a63c1280b7e99e103cdf9c4122ba5e2e8c
Joey <joey@evolution-x.org>
2025-11-14 15:01:45 +0900

    Revert "InputDispatcher: On keypress, deliver keycode to pokeUserActivity"
    This reverts commit 5702408b2d35e81d8ad9b76a1d83c45a7b7f2a7e.

--- services/inputflinger/dispatcher/InputDispatcher.cpp
+++ services/inputflinger/dispatcher/InputDispatcher.cpp
@@ -3270,7 +3270,6 @@ void InputDispatcher::pokeUserActivityLocked(const EventEntry& eventEntry) {
         }
     }
 
-    int32_t keyCode = AKEYCODE_UNKNOWN;
     switch (eventEntry.type) {
         case EventEntry::Type::MOTION: {
             const MotionEntry& motionEntry = static_cast<const MotionEntry&>(eventEntry);
@@ -3299,8 +3298,6 @@ void InputDispatcher::pokeUserActivityLocked(const EventEntry& eventEntry) {
                         << windowDisablingUserActivityInfo->name << "'.";
                 return;
             }
-
-            keyCode = keyEntry.keyCode;
             break;
         }
         default: {
@@ -3311,10 +3308,10 @@ void InputDispatcher::pokeUserActivityLocked(const EventEntry& eventEntry) {
     }
 
     mLastUserActivityTimes[eventType] = eventEntry.eventTime;
-    auto command = [this, eventTime = eventEntry.eventTime, eventType, displayId, keyCode]()
+    auto command = [this, eventTime = eventEntry.eventTime, eventType, displayId]()
                            REQUIRES(mLock) {
                                scoped_unlock unlock(mLock);
-                               mPolicy.pokeUserActivity(eventTime, eventType, displayId, keyCode);
+                               mPolicy.pokeUserActivity(eventTime, eventType, displayId);
                            };
     postCommandLocked(std::move(command));
 }
